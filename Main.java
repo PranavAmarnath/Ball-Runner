be01d6d68f9028c30402c22158d432aeacb8d7de
PranavAmarnath <64337291+PranavAmarnath@users.noreply.github.com>
2020-05-03 13:48:53 -0700

    next commit with adjustment to ball

--- Main.java
+++ Main.java
@@ -3,24 +3,24 @@ public class Main {
     public static boolean gameOn = true;
     public static int score;
     public static int ball_x = 20;
-    public static int ball_y = 700;
+    public static int ball_y = 850;
     private static int steps = 20;
     public static DrawingBoard board = new DrawingBoard(1920, 1000);
     public static Ball ball = new Ball(ball_x, ball_y, 20, 10);
 
     public static void main(String[] args) {
-        board.getJFrame().addKeyListener(ball);
         board.clear();
         ball.draw();
+        board.addKeyListener(ball);
         run();
     }
 
     public static void run() {
-        board.getJFrame().addKeyListener(ball);
+        board.addKeyListener(ball);
         for (int i = 0; i < steps; i++) {
             ball.move();
             try {
-                Thread.sleep(40);
+                Thread.sleep(30);
             } catch (InterruptedException e) {
             }
             board.clear();
@@ -30,7 +30,7 @@ public class Main {
         for (int i = 0; i < steps; i++) {
             ball.moveBack();
             try {
-                Thread.sleep(40);
+                Thread.sleep(30);
             } catch (InterruptedException e) {
             }
             board.clear();
