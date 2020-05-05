54a9a4229792990a0fa36269a8221b835dcb9366
PranavAmarnath <64337291+PranavAmarnath@users.noreply.github.com>
2020-05-04 17:07:10 -0700

    commit to resize output window

--- Main.java
+++ Main.java
@@ -2,16 +2,20 @@ public class Main {
 
     public static boolean gameOn = true;
     public static int score;
+    private static final int w = 1728;
+    private static final int h = 972;
     public static int ball_x = 50;
-    public static int ball_y = 850;
+    public static int ball_y = (h - 150);
     private static int steps = 40;
-    public static DrawingBoard board = new DrawingBoard(1920, 1000);
-    public static Ball ball = new Ball(ball_x, ball_y, 20, 5);
+    public static DrawingBoard board;
+    public static Ball ball;
 
     // Loading Problem at beginning -> have to fix
     // Need to synchronize spacebar with ballRun();
 
     public static void main(String[] args) {
+        board = new DrawingBoard(w, h);
+        ball = new Ball(ball_x, ball_y, 20, 5);
         //board.clear();
         ballRun();
         ballRun();
