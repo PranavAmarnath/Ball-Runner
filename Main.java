a823d780e3ce790cfd2e00b709fe10e5affe7ec8
Aditya <aditya26042005@gmail.com>
2020-05-08 13:35:26 -0700

    obstacle testing

--- Main.java
+++ Main.java
@@ -11,8 +11,8 @@ public class Main {
     private static final int h = 576;
     public static int ball_x = 50;
     public static int ball_y = (h - 150);
+    public static int obstacle_width = 50;
     public static int obstacle_height = 100;
-    public static int obstacle_width = obstacle_height/2;
     public static DrawingBoard board;
     public static Ball ball;
     public static Obstacle obstacle;
@@ -20,18 +20,20 @@ public class Main {
     //private static File boing;
 
     public static void main(String[] args) {
+        Thread obstacleThread = new Thread(obstacles);
+        obstacleThread.start();
         board = new DrawingBoard(w, h);
         ball = new Ball(ball_x, ball_y, 15, 200);
         sound = new SoundPlayer();
         Thread t = new Thread(sound);
         t.start();
         //boing = new File("boing.wav");
-        obstacle = new Obstacle(800, 350, obstacle_width, obstacle_height);
+        //obstacle = new Obstacle(800, 350, obstacle_width, obstacle_height);
         board.getJFrame().addKeyListener(ball);
         while(gameOn) {
             board.clear();
             ball.draw();
-            obstacle.drawOb(DrawingBoard.getCanvas());
+            obstacle.drawOb(DrawingBoard.getCanvas());;
             board.repaint();
             while (ball.ballOn) {
                 //sound.playSound(boing);
---
+++ ObstacleManager.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class ObstacleManager implements Runnable{
+
+    public void run() {
+        return;
+    }
+
+    Obstacle obstacles = new Obstacle();
+    
+    
+    //obstacles[1] = new Obstacle(800, 350, obstacle_width, obstacle_height);
+
+}
